8bb36c045eda81e29acd2881aef38fd340ae2f2d
Wang Yimin <wangyimin@yahoo.com>
2024-01-28 11:26:12 +0900

    ps1

---
+++ sample.ps1
@@ -0,0 +1,37 @@
+﻿
+$triggers = Get-ChildItem "HKLM:\SYSTEM\CurrentControlSet\Services" |
+    ?{ $_.GetSubkeyNames().Contains("TriggerInfo") } |
+    %{ $_.Name.Split("\")[-1] }
+
+$startMode = @{ Manual = "手動"; Disabled = "無効"; Auto = "自動"; Unknown = "不明" }
+$startOption = @{ 01 = " (トリガー開始)"; 10 = " (遅延開始)"; 11 = " (遅延開始、トリガー開始)" }
+
+$serviceData = Get-CimInstance -ClassName Win32_Service | select @(
+    @{ n = "表示名";              e = { $_.DisplayName } }
+    @{ n = "サービス名";          e = { $_.Name } }
+    @{ n = "スタートアップの種類"; e = { $startMode[$_.StartMode] + $startOption[10 * ($_.StartMode -eq "Auto" -and $_.DelayedAutoStart) + $triggers.Contains($_.Name)] } }
+    @{ n = "状態";                e = { if($_.State -eq "Running") { "実行" } else { "停止" } } }
+)
+$serviceData
+
+Function ListFirewallRuleInbound($names){
+    if (!($names -is [Array])){
+        throw "Invalad parameter"
+    }
+    
+    $fw = @()
+    Get-Netfirewallrule -Enabled True -Direction "Inbound" | where DisplayName -in $names | foreach{
+        $el = [PSCustomObject]@{
+            "app" = $_ | NetFirewallApplicationFilter | select "Program";
+            "addr" = $_ | Get-NetfirewallAddressFilter | select "LocalAddress", "RemoteAddress";
+            "port" = $_ | Get-NetFirewallPortFilter | select "LocalPort", "RemotePort";
+        };
+        $fw += $el;
+    }
+
+    return $fw;
+}
+
+$f = $function:ListFirewallRuleInbound
+& $f @("code.exe") | Format-Table
+#ListFirewallRuleInbound(@("code.exe"))
